b887e40de81db30ba6cd18a2ce5917280bf5e1ae
rllyhz <rullyihza00@gmail.com>
2021-04-19 13:30:08 +0700

    Creating Detail UI

---
+++ app/src/main/java/id/rllyhz/sunglassesshow/ui/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package id.rllyhz.sunglassesshow.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.rllyhz.sunglassesshow.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
