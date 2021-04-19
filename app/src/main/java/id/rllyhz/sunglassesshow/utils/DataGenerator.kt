e881f7c686c4c65f6a017f1ad2982a9a0658d2fd
rllyhz <rullyihza00@gmail.com>
2021-04-19 15:56:40 +0700

    Updating DataGenerator.kt file

--- app/src/main/java/id/rllyhz/sunglassesshow/utils/DataGenerator.kt
+++ app/src/main/java/id/rllyhz/sunglassesshow/utils/DataGenerator.kt
@@ -1,7 +1,27 @@
 package id.rllyhz.sunglassesshow.utils
 
-object DataGenerator {
-    private fun loadAssetFile(filename: String) {
+import android.app.Activity
+import android.content.Context
+import java.io.IOException
 
+object DataGenerator {
+    private val MOVIES_FILENAME = "movies.json"
+    private val TV_SHOW_FILENAME = "tv_show.json"
+
+    private fun loadAssetFile(context: Context, filename: String): String? {
+        val jsonString: String
+
+        try {
+            jsonString = context.assets.open(filename).bufferedReader().use { it.readText() }
+        } catch (ioException: IOException) {
+            ioException.printStackTrace()
+            return null
+        }
+
+        return jsonString
+    }
+
+    fun getAllMovies(context: Activity) {
+        val jsonData = loadAssetFile(context, MOVIES_FILENAME)
     }
 }
