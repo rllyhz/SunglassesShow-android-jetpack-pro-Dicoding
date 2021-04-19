d5604cdfe172d89639272e6d8222fb006354ae04
rllyhz <rullyihza00@gmail.com>
2021-04-19 11:10:29 +0700

    Initial commit

---
+++ app/src/main/java/id/rllyhz/sunglassesshow/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package id.rllyhz.sunglassesshow.ui.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import id.rllyhz.sunglassesshow.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
