b9dbbfa23f6d8641b7ee123f7f4b69afafe874e0
rllyhz <rullyihza00@gmail.com>
2021-04-19 11:30:53 +0700

    Updating Main UI

--- app/src/main/java/id/rllyhz/sunglassesshow/ui/main/MainActivity.kt
+++ app/src/main/java/id/rllyhz/sunglassesshow/ui/main/MainActivity.kt
@@ -1,12 +1,25 @@
 package id.rllyhz.sunglassesshow.ui.main
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import id.rllyhz.sunglassesshow.R
+import id.rllyhz.sunglassesshow.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupUI()
+    }
+
+    private fun setupUI() {
+        binding.apply {
+            progressbar.visibility = View.VISIBLE
+        }
     }
 }
