e251f274ed10812f9d363a0ac501b531685af6b7
rllyhz <rullyihza00@gmail.com>
2021-04-19 13:08:01 +0700

    Creating Movie Model

---
+++ app/src/main/java/id/rllyhz/sunglassesshow/data/Movie.kt
@@ -0,0 +1,15 @@
+package id.rllyhz.sunglassesshow.data
+
+data class Movie(
+    val title: String,
+    val posterUrl: String,
+    val year: Int,
+    val releashedAt: String,
+    var duration: String,
+    val rate: String,
+    val genres: String,
+    val languages: String,
+    val synopsis: String,
+    val director: String,
+    val userScore: String
+)
