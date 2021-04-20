4d5942805aaea1f047a50b1f9fe63cf4c5b19b67
rllyhz <rullyihza00@gmail.com>
2021-04-20 21:52:02 +0700

    Adding testing methods in MainActivityTest.kt

--- app/src/androidTest/java/id/rllyhz/sunglassesshow/ui/main/MainActivityTest.kt
+++ app/src/androidTest/java/id/rllyhz/sunglassesshow/ui/main/MainActivityTest.kt
@@ -1,12 +1,72 @@
 package id.rllyhz.sunglassesshow.ui.main
 
-import org.junit.Before
-
-import org.junit.Assert.*
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.swipeLeft
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import id.rllyhz.sunglassesshow.R
+import id.rllyhz.sunglassesshow.utils.DataGenerator
+import org.junit.Rule
+import org.junit.Test
 
 class MainActivityTest {
+    private val moviesDummyData = DataGenerator.getAllMovies()
+    private val tvShowsDummyData = DataGenerator.getAllTVShows()
+    private val positionItemForTesting = 1
 
-    @Before
-    fun setUp() {
+    @get:Rule
+    var activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun loadAndShowDataMoviesAndMakeSureTheyAreClickable() {
+
+        onView(withId(R.id.rv_movie_list)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.rv_movie_list)).perform(
+            RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
+                moviesDummyData.size
+            )
+        )
+
+        onView(withId(R.id.rv_movie_list)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                positionItemForTesting,
+                click()
+            )
+        )
+    }
+
+    @Test
+    fun swipeToTvShowsPreview() {
+        onView(withId(R.id.view_pager_main)).check(matches(isDisplayed()))
+        onView(withId(R.id.view_pager_main)).perform(swipeLeft())
+        onView(withId(R.id.rv_tvshow_list)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun loadAndShowDataTVShowsAndMakeSureTheyAreClickable() {
+        onView(withId(R.id.view_pager_main)).check(matches(isDisplayed()))
+        onView(withId(R.id.view_pager_main)).perform(swipeLeft())
+        onView(withId(R.id.rv_tvshow_list)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.rv_tvshow_list)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.rv_tvshow_list)).perform(
+            RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
+                tvShowsDummyData.size
+            )
+        )
+
+        onView(withId(R.id.rv_tvshow_list)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                positionItemForTesting,
+                click()
+            )
+        )
     }
 }
