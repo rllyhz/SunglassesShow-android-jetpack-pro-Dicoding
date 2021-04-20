cec96a95375e46dd122b69c6da193451e67081e2
rllyhz <rullyihza00@gmail.com>
2021-04-20 10:41:42 +0700

    Adding getAllMovies and getAllTVShows methods in DataGenerator.kt file

--- app/src/main/java/id/rllyhz/sunglassesshow/utils/DataGenerator.kt
+++ app/src/main/java/id/rllyhz/sunglassesshow/utils/DataGenerator.kt
@@ -1,12 +1,14 @@
 package id.rllyhz.sunglassesshow.utils
 
-import android.app.Activity
 import android.content.Context
+import com.google.gson.Gson
+import id.rllyhz.sunglassesshow.data.Movie
+import id.rllyhz.sunglassesshow.data.TVShow
 import java.io.IOException
 
 object DataGenerator {
-    private val MOVIES_FILENAME = "movies.json"
-    private val TV_SHOW_FILENAME = "tv_show.json"
+    private const val MOVIES_FILENAME = "movies.json"
+    private const val TV_SHOW_FILENAME = "tv_show.json"
 
     private fun loadAssetFile(context: Context, filename: String): String? {
         val jsonString: String
@@ -21,7 +23,19 @@ object DataGenerator {
         return jsonString
     }
 
-    fun getAllMovies(context: Activity) {
-        val jsonData = loadAssetFile(context, MOVIES_FILENAME)
+    fun getAllMovies(context: Context): Array<Movie>? {
+        loadAssetFile(context, MOVIES_FILENAME)?.run {
+            return Gson().fromJson(this, Array<Movie>::class.java)
+        }
+
+        return null
+    }
+
+    fun getAllTVShows(context: Context): Array<TVShow>? {
+        loadAssetFile(context, TV_SHOW_FILENAME)?.run {
+            return Gson().fromJson(this, Array<TVShow>::class.java)
+        }
+
+        return null
     }
 }
