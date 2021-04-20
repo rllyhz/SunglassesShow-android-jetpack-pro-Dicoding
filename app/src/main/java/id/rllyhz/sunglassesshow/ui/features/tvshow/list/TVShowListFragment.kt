3022318a619f09943e86891dc81a8a311430c1d3
rllyhz <rullyihza00@gmail.com>
2021-04-20 12:04:07 +0700

    Updating TVShowListFragment.kt

--- app/src/main/java/id/rllyhz/sunglassesshow/ui/features/tvshow/list/TVShowListFragment.kt
+++ app/src/main/java/id/rllyhz/sunglassesshow/ui/features/tvshow/list/TVShowListFragment.kt
@@ -5,12 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import id.rllyhz.sunglassesshow.databinding.FragmentTvshowListBinding
+import id.rllyhz.sunglassesshow.ui.main.MainViewModel
 
 class TVShowListFragment : Fragment() {
     private var _binding: FragmentTvshowListBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: MainViewModel by viewModels()
+    private lateinit var tvShowListAdapter: TVShowListAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,14 +28,21 @@ class TVShowListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        tvShowListAdapter = TVShowListAdapter()
         setupUI()
     }
 
     private fun setupUI() {
+        tvShowListAdapter.submitList(viewModel.getTVShows().toMutableList())
+
         binding.apply {
+            progressbar.visibility = View.GONE
+
             with(rvTvshowList) {
                 layoutManager = GridLayoutManager(context, 2)
                 setHasFixedSize(true)
+                adapter = tvShowListAdapter
             }
         }
     }
@@ -38,5 +50,10 @@ class TVShowListFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        val TAG: String = TVShowListFragment::class.java.simpleName
+        fun newInstance(): TVShowListFragment = TVShowListFragment()
     }
 }
