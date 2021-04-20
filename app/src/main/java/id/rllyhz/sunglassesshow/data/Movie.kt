d63e4163c18904e9a0131bff7223e894ce9a32fe
rllyhz <rullyihza00@gmail.com>
2021-04-20 10:49:50 +0700

    Updating Movie and TV Show models

--- app/src/main/java/id/rllyhz/sunglassesshow/data/Movie.kt
+++ app/src/main/java/id/rllyhz/sunglassesshow/data/Movie.kt
@@ -1,21 +1,25 @@
 package id.rllyhz.sunglassesshow.data
 
 import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class Movie(
     val id: Int,
+    @SerializedName("poster_url")
     val posterUrl: String,
     val title: String,
     val year: Int,
     val genres: String,
     val duration: String,
     val rate: String,
+    @SerializedName("released_at")
     val releasedAt: String,
     val language: String,
+    @SerializedName("user_score")
     val userScore: String,
-    val synopsis: String,
     val tagline: String,
+    val synopsis: String,
     val director: String
 ) : Parcelable
