8942c75195ba472fddf3d89f713969ed7f241db1
rllyhz <rullyihza00@gmail.com>
2021-04-20 16:13:33 +0700

    Updating DetailActivity.kt

--- app/src/main/java/id/rllyhz/sunglassesshow/ui/detail/DetailActivity.kt
+++ app/src/main/java/id/rllyhz/sunglassesshow/ui/detail/DetailActivity.kt
@@ -1,6 +1,7 @@
 package id.rllyhz.sunglassesshow.ui.detail
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import id.rllyhz.sunglassesshow.R
@@ -39,6 +40,8 @@ class DetailActivity : AppCompatActivity() {
 
             setupUI()
         }
+
+        setupActionBar()
     }
 
     private fun setupUI() {
@@ -69,6 +72,20 @@ class DetailActivity : AppCompatActivity() {
             .add(R.id.fl_fragment_container_detail, activeFragment, tag)
             .addToBackStack(null)
             .commit()
+    }
+
+    private fun setupActionBar() {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> onOptionsItemSelected(item)
+        }
     }
 
     companion object {
