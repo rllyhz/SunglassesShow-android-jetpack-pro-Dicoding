002b56d18f3245cc4ff680bf0f663123a0097b8b
rllyhz <rullyihza00@gmail.com>
2021-04-20 12:53:37 +0700

    Adding rating provider's logic in Movie and TVShow models

--- app/src/main/java/id/rllyhz/sunglassesshow/data/TVShow.kt
+++ app/src/main/java/id/rllyhz/sunglassesshow/data/TVShow.kt
@@ -22,4 +22,22 @@ data class TVShow(
     val tagline: String,
     val synopsis: String,
     val creator: String
-) : Parcelable
+) : Parcelable {
+
+    val rating: Float
+        get() = userScore.replace("%", "").toInt().let { score ->
+            return when {
+                score <= 10 -> 1f
+                score <= 20 -> 1.5f
+                score <= 30 -> 2f
+                score <= 40 -> 2.5f
+                score <= 50 -> 3f
+                score <= 60 -> 3.4f
+                score <= 70 -> 3.8f
+                score <= 80 -> 4f
+                score <= 90 -> 4.5f
+                score <= 100 -> 5f
+                else -> 0f
+            }
+        }
+}
