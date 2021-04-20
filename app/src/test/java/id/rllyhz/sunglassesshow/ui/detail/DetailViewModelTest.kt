c85450d3e14a7684a83b932b37a67da69c19c237
rllyhz <rullyihza00@gmail.com>
2021-04-20 16:33:05 +0700

    Creating DetailViewModelTest.kt for testing

---
+++ app/src/test/java/id/rllyhz/sunglassesshow/ui/detail/DetailViewModelTest.kt
@@ -0,0 +1,42 @@
+package id.rllyhz.sunglassesshow.ui.detail
+
+import id.rllyhz.sunglassesshow.data.Movie
+import id.rllyhz.sunglassesshow.data.TVShow
+import id.rllyhz.sunglassesshow.ui.main.MainViewModel
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Before
+import org.junit.Test
+
+class DetailViewModelTest {
+    private lateinit var viewModel: DetailViewModel
+    private lateinit var mainViewModel: MainViewModel
+    private lateinit var currentMovie: Movie
+    private lateinit var currentTVShow: TVShow
+
+    @Before
+    fun setUp() {
+        mainViewModel = MainViewModel()
+        viewModel = DetailViewModel()
+        currentMovie = mainViewModel.getMovies()[0]
+        currentTVShow = mainViewModel.getTVShows()[0]
+    }
+
+    @Test
+    fun getSimilarMovieOf() {
+        val similarMovies = viewModel.getSimilarMovieOf(currentMovie)
+        val expectedSize = 6
+
+        assertNotNull(similarMovies)
+        assertEquals(expectedSize, similarMovies.size)
+    }
+
+    @Test
+    fun getSimilarTVShowOf() {
+        val similarTVShows = viewModel.getSimilarTVShowOf(currentTVShow)
+        val expectedSize = 6
+
+        assertNotNull(similarTVShows)
+        assertEquals(expectedSize, similarTVShows.size)
+    }
+}
