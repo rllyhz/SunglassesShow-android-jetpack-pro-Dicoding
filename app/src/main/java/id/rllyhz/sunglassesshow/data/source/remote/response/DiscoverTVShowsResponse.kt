d05f23f34ffb21a0b106f0ef969417b43222cf88
rllyhz <rullyihza00@gmail.com>
2021-05-01 21:38:49 +0700

    Updating DiscoverTVShowsResponse.kt

--- app/src/main/java/id/rllyhz/sunglassesshow/data/source/remote/response/DiscoverTVShowsResponse.kt
+++ app/src/main/java/id/rllyhz/sunglassesshow/data/source/remote/response/DiscoverTVShowsResponse.kt
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName
 data class DiscoverTVShowsResponse(
     val page: Int,
     @SerializedName("results")
-    val movies: List<TVShowResponse>,
+    val tvShows: List<TVShowResponse>,
     @SerializedName("total_pages")
     val totalPages: Int,
 )
