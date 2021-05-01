3b08ada7d469f61fab61e92e6cb937f2488d68bf
rllyhz <rullyihza00@gmail.com>
2021-05-01 20:23:30 +0700

    Updating Movie.kt and TVShow.kt files

--- app/src/main/java/id/rllyhz/sunglassesshow/data/TVShow.kt
+++ app/src/main/java/id/rllyhz/sunglassesshow/data/TVShow.kt
@@ -1,42 +1,45 @@
 package id.rllyhz.sunglassesshow.data
 
+import android.os.Build
 import android.os.Parcelable
-import com.google.gson.annotations.SerializedName
+import androidx.annotation.RequiresApi
 import kotlinx.android.parcel.Parcelize
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 @Parcelize
 data class TVShow(
     val id: Int,
-    @SerializedName("poster_name")
     val posterName: String,
     val title: String,
-    val year: Int,
-    val genres: String,
-    val duration: String,
-    val rate: String,
-    @SerializedName("released_at")
+    val genres: String?,
+    val duration: String?,
+    val rate: Float,
     val releasedAt: String,
     val language: String,
-    @SerializedName("user_score")
-    val userScore: String,
-    val tagline: String,
+    val tagline: String?,
     val synopsis: String,
-    val creator: String
+    val creator: String?
 ) : Parcelable {
 
+    val year: Int
+        @RequiresApi(Build.VERSION_CODES.O)
+        get() =
+            LocalDate.parse(releasedAt, DateTimeFormatter.ISO_DATE).year
+
     val rating: Float
-        get() = userScore.replace("%", "").toInt().let { score ->
+        get() = rate.let { score ->
             return when {
-                score <= 10 -> 1f
-                score <= 20 -> 1.5f
-                score <= 30 -> 2f
-                score <= 40 -> 2.5f
-                score <= 50 -> 3f
-                score <= 60 -> 3.4f
-                score <= 70 -> 3.8f
-                score <= 80 -> 4f
-                score <= 90 -> 4.5f
-                score <= 100 -> 5f
+                score <= 1 -> 1f
+                score <= 2 -> 1.5f
+                score <= 3 -> 2f
+                score <= 4 -> 2.5f
+                score <= 5 -> 3f
+                score <= 6 -> 3.4f
+                score <= 7 -> 3.8f
+                score <= 8 -> 4f
+                score <= 9 -> 4.5f
+                score <= 10 -> 5f
                 else -> 0f
             }
         }
