f3cfde03dd71f6a01e698c4acc803aa28ab72901
rllyhz <rullyihza00@gmail.com>
2021-05-01 20:55:06 +0700

    Updating MovieDetailResponse.kt

--- app/src/main/java/id/rllyhz/sunglassesshow/data/source/remote/response/MovieDetailResponse.kt
+++ app/src/main/java/id/rllyhz/sunglassesshow/data/source/remote/response/MovieDetailResponse.kt
@@ -11,12 +11,15 @@ data class MovieDetailResponse(
     val title: String,
     val tagline: String,
     val status: String,
+    val genres: List<GenreResonse>,
     @SerializedName("overview")
     val synopsis: String,
     @SerializedName("release_date")
     val releasedDate: String,
     @SerializedName("runtime")
     val duration: Int,
+    @SerializedName("origin_language")
+    val language: String,
     @SerializedName("vote_average")
     val rate: Float
 )
