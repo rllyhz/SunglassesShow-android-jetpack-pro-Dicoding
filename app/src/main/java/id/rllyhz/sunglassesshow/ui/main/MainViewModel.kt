8b835aa9c5def1729d82aa1295b3fc30ba536914
rllyhz <rullyihza00@gmail.com>
2021-05-02 14:54:04 +0700

    Updating MainViewModel.kt file

--- app/src/main/java/id/rllyhz/sunglassesshow/ui/main/MainViewModel.kt
+++ app/src/main/java/id/rllyhz/sunglassesshow/ui/main/MainViewModel.kt
@@ -1,24 +1,14 @@
 package id.rllyhz.sunglassesshow.ui.main
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import id.rllyhz.sunglassesshow.data.Movie
 import id.rllyhz.sunglassesshow.data.TVShow
 import id.rllyhz.sunglassesshow.data.source.SunGlassesShowRepository
 import id.rllyhz.sunglassesshow.utils.DataGenerator
-import id.rllyhz.sunglassesshow.utils.ResourceEvent
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 
 class MainViewModel(
     private val repository: SunGlassesShowRepository
 ) : ViewModel() {
-    private val _state = MutableStateFlow<ResourceEvent>(ResourceEvent.Empty)
-    val movieState: StateFlow<ResourceEvent> = _state
-
-    private val _movies = MutableLiveData<List<Movie>>()
-    val movies: LiveData<List<Movie>> = _movies
 
     suspend fun getMoviesTest() =
         repository.getMovies()
