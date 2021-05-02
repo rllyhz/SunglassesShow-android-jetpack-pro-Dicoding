b2ebbeb78b34bd4f5926cec206abb735b982e426
rllyhz <rullyihza00@gmail.com>
2021-05-02 16:48:27 +0700

    Adding abstract methods in SunGlassesShowDataSource.kt file

--- app/src/main/java/id/rllyhz/sunglassesshow/data/source/SunGlassesShowDataSource.kt
+++ app/src/main/java/id/rllyhz/sunglassesshow/data/source/SunGlassesShowDataSource.kt
@@ -1,5 +1,20 @@
 package id.rllyhz.sunglassesshow.data.source
 
-interface SunGlassesShowDataSource {
+import androidx.lifecycle.LiveData
+import id.rllyhz.sunglassesshow.data.Movie
+import id.rllyhz.sunglassesshow.data.TVShow
+import id.rllyhz.sunglassesshow.utils.Resource
 
+interface SunGlassesShowDataSource {
+    suspend fun getMovies(): LiveData<Resource<List<Movie>>>
+
+    suspend fun getTVShows(): LiveData<Resource<List<TVShow>>>
+
+    suspend fun getDetailMovieOf(id: Int): LiveData<Resource<Movie>>
+
+    suspend fun getDetailTVShowOf(id: Int): LiveData<Resource<TVShow>>
+
+    suspend fun getSimilarMoviesOf(id: Int): LiveData<Resource<List<Movie>>>
+
+    suspend fun getSimilarTVShowsOf(id: Int): LiveData<Resource<List<TVShow>>>
 }
