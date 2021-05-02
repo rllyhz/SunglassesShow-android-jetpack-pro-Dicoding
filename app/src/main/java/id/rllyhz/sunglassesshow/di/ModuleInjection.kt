618c853df94da0d604f02ae0b42735431df14ec6
rllyhz <rullyihza00@gmail.com>
2021-05-02 17:13:26 +0700

    Updating ModuleInjection.kt file

--- app/src/main/java/id/rllyhz/sunglassesshow/di/ModuleInjection.kt
+++ app/src/main/java/id/rllyhz/sunglassesshow/di/ModuleInjection.kt
@@ -1,19 +1,26 @@
 package id.rllyhz.sunglassesshow.di
 
 import id.rllyhz.sunglassesshow.api.ApiEndpoint
+import id.rllyhz.sunglassesshow.api.ApiEndpoint.Companion.BASE_URL
 import id.rllyhz.sunglassesshow.data.source.SunGlassesShowRepository
 import id.rllyhz.sunglassesshow.data.source.remote.RemoteDataSource
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ModuleInjection {
+    @Volatile
+    private var apiInstance: Retrofit? = null
 
-    fun provideApiClient(): ApiEndpoint =
-        Retrofit.Builder()
-            .baseUrl(ApiEndpoint.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(ApiEndpoint::class.java)
+    private fun provideApiInstance(): Retrofit =
+        apiInstance ?: synchronized(this) {
+            apiInstance ?: Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+
+    fun provideApiClient(): ApiEndpoint = provideApiInstance()
+        .create(ApiEndpoint::class.java)
 
     fun provideMainRepository(): SunGlassesShowRepository =
         SunGlassesShowRepository.getInstance(
