05ac0f1260f81c9b82c489cd8adb061bfeb7cff8
rllyhz <rullyihza00@gmail.com>
2021-05-03 16:28:22 +0700

    Removing all methods from the previous submission in DetailViewModelTest.kt and MainViewModelTest.kt files

--- app/src/test/java/id/rllyhz/sunglassesshow/ui/detail/DetailViewModelTest.kt
+++ app/src/test/java/id/rllyhz/sunglassesshow/ui/detail/DetailViewModelTest.kt
@@ -2,9 +2,8 @@ package id.rllyhz.sunglassesshow.ui.detail
 
 import id.rllyhz.sunglassesshow.data.Movie
 import id.rllyhz.sunglassesshow.data.TVShow
+import id.rllyhz.sunglassesshow.di.ModuleInjection
 import id.rllyhz.sunglassesshow.ui.main.MainViewModel
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNotNull
 import org.junit.Before
 import org.junit.Test
 
@@ -16,37 +15,25 @@ class DetailViewModelTest {
 
     @Before
     fun setUp() {
-        mainViewModel = MainViewModel()
-        viewModel = DetailViewModel()
-        currentMovie = mainViewModel.getMovies()[0]
-        currentTVShow = mainViewModel.getTVShows()[0]
+        mainViewModel = MainViewModel(ModuleInjection.provideMainRepository())
+        viewModel = DetailViewModel(ModuleInjection.provideMainRepository())
     }
 
     @Test
     fun `currentMovie must not be null`() {
-        assertNotNull(currentMovie)
+
     }
 
     @Test
     fun `currentTVShow must not be null`() {
-        assertNotNull(currentTVShow)
+
     }
 
     @Test
     fun `get all similar movie data, they can not be null and has the correct size`() {
-        val similarMovies = viewModel.getSimilarMovieOf(currentMovie)
-        val expectedSize = 6
-
-        assertNotNull(similarMovies)
-        assertEquals(expectedSize, similarMovies.size)
     }
 
     @Test
     fun `get all similar tv show data, they can not be null and has the correct size`() {
-        val similarTVShows = viewModel.getSimilarTVShowOf(currentTVShow)
-        val expectedSize = 6
-
-        assertNotNull(similarTVShows)
-        assertEquals(expectedSize, similarTVShows.size)
     }
 }
--- app/src/test/java/id/rllyhz/sunglassesshow/ui/main/MainViewModelTest.kt
+++ app/src/test/java/id/rllyhz/sunglassesshow/ui/main/MainViewModelTest.kt
@@ -1,33 +1,13 @@
 package id.rllyhz.sunglassesshow.ui.main
 
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNotNull
+import id.rllyhz.sunglassesshow.di.ModuleInjection
 import org.junit.Before
-import org.junit.Test
 
 class MainViewModelTest {
     private lateinit var viewModel: MainViewModel
 
     @Before
     fun setUp() {
-        viewModel = MainViewModel()
-    }
-
-    @Test
-    fun `get all movies data, they can not be null and has the correct size`() {
-        val movies = viewModel.getMovies()
-        val expectedSize = 17
-
-        assertNotNull(movies)
-        assertEquals(expectedSize, movies.size)
-    }
-
-    @Test
-    fun `get all tv shows data, they can not be null and has the correct size`() {
-        val tvShows = viewModel.getTVShows()
-        val expectedSize = 19
-
-        assertNotNull(tvShows)
-        assertEquals(expectedSize, tvShows.size)
+        viewModel = MainViewModel(ModuleInjection.provideMainRepository())
     }
 }
