c4368ba07a39f5c8572eea0bfeebd23a6a54df65
rllyhz <rullyihza00@gmail.com>
2021-05-04 22:00:27 +0700

    Updating MainActivityTest.kt

--- app/src/androidTest/java/id/rllyhz/sunglassesshow/ui/main/MainActivityTest.kt
+++ app/src/androidTest/java/id/rllyhz/sunglassesshow/ui/main/MainActivityTest.kt
@@ -13,6 +13,7 @@ import androidx.test.platform.app.InstrumentationRegistry
 import id.rllyhz.sunglassesshow.R
 import id.rllyhz.sunglassesshow.utils.DataGenerator
 import id.rllyhz.sunglassesshow.utils.EspressoIdlingResource
+import id.rllyhz.sunglassesshow.utils.getDateInString
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -21,8 +22,8 @@ import org.junit.Test
 class MainActivityTest {
     private val moviesDummyData = DataGenerator.getAllMovies()
     private val tvShowsDummyData = DataGenerator.getAllTVShows()
-    private val positionItemForTesting = 1
-    private val itemTesting = moviesDummyData[positionItemForTesting]
+    private val positionItemForTesting = 0
+    private val movieItemTesting = DataGenerator.getDetailMovie()
     private val similarContents = DataGenerator.getSimilarMovies()
 
     @get:Rule
@@ -39,7 +40,7 @@ class MainActivityTest {
     }
 
     @Test
-    fun loadAndShowDataMoviesAndMakeSureTheyAreClickable() {
+    fun loadAndShowDataMovies_makeSureTheyAreClickable() {
 
         onView(withId(R.id.rv_movie_list)).check(matches(isDisplayed()))
 
@@ -58,7 +59,7 @@ class MainActivityTest {
     }
 
     @Test
-    fun swipeToTvShowsTabPreview() {
+    fun swipeToTheLeftTvShowsTabPreview() {
         onView(withId(R.id.view_pager_main)).check(matches(isDisplayed()))
         onView(withId(R.id.view_pager_main)).perform(swipeLeft())
         onView(withId(R.id.rv_tvshow_list)).check(matches(isDisplayed()))
@@ -85,7 +86,7 @@ class MainActivityTest {
     }
 
     @Test
-    fun detailActivitySimulationAndMakeSureEverythingOnItDisplayedAndClickable() {
+    fun detailActivitySimulation_makeSureEverythingOnItDisplayedAndClickable() {
         onView(withId(R.id.rv_movie_list)).check(matches(isDisplayed()))
         onView(withId(R.id.rv_movie_list)).perform(
             RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
@@ -141,31 +142,37 @@ class MainActivityTest {
         }
 
     @Test
-    fun detailActivitySimulationAndMakeSureEveryTextViewComponentsShowTheCorrectText() {
+    fun detailActivitySimulation_makeSureEveryTextViewComponentsShowTheCorrectText() {
         val context = InstrumentationRegistry.getInstrumentation().targetContext
 
         val expectedTitle = getStringResources(
             context,
             R.string.title_format,
-            itemTesting.title,
-            itemTesting.year.toString()
+            movieItemTesting.title,
+            movieItemTesting.year.toString()
         )
 
         val expectedRate = getStringResources(
             context,
             R.string.rate_format,
-            itemTesting.rate.toString()
+            movieItemTesting.rate.toString()
         )
 
-        val expectedDirector = getStringResources(
+        val expectedStatus = getStringResources(
             context,
             R.string.status_format,
-            itemTesting.status
+            movieItemTesting.status ?: ""
         )
 
-        val expectedGenres = itemTesting.genres
-        val expectedSynopsis = itemTesting.synopsis
-        val expectedDuration = itemTesting.duration
+        val expectedReleasedAt = getStringResources(
+            context,
+            R.string.released_at_format,
+            movieItemTesting.getDateInString()
+        )
+
+        val expectedGenres = movieItemTesting.genres
+        val expectedSynopsis = movieItemTesting.synopsis
+        val expectedDuration = movieItemTesting.duration
 
         onView(withId(R.id.rv_movie_list)).check(matches(isDisplayed()))
         onView(withId(R.id.rv_movie_list)).perform(
@@ -176,10 +183,36 @@ class MainActivityTest {
         )
 
         onView(withId(R.id.tv_title_detail)).check(matches(withText(expectedTitle)))
-        //onView(withId(R.id.tv_genres_detail)).check(matches(withText(expectedGenres)))
+        onView(withId(R.id.tv_genres_detail)).check(matches(withText(expectedGenres)))
         onView(withId(R.id.tv_rate_detail)).check(matches(withText(expectedRate)))
-        //onView(withId(R.id.tv_status_detail)).check(matches(withText(expectedDirector)))
-        //onView(withId(R.id.tv_synopsis_detail)).check(matches(withText(expectedSynopsis)))
-        //onView(withId(R.id.tv_duration_detail)).check(matches(withText(expectedDuration)))
+        onView(withId(R.id.tv_released_at_detail)).check(matches(withText(expectedReleasedAt)))
+        onView(withId(R.id.tv_status_detail)).check(matches(withText(expectedStatus)))
+        onView(withId(R.id.tv_synopsis_detail)).check(matches(withText(expectedSynopsis)))
+        onView(withId(R.id.tv_duration_detail)).check(matches(withText(expectedDuration)))
+    }
+
+    @Test
+    fun detailActivitySimulationAndThenLoadAndShowSimilarMovies_makeSureTheyAreClickable() {
+        onView(withId(R.id.rv_movie_list)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_movie_list)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                positionItemForTesting,
+                click()
+            )
+        )
+
+        onView(withId(R.id.rv_similar_content_detail)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_similar_content_detail)).perform(
+            RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
+                similarContents.size
+            )
+        )
+
+        onView(withId(R.id.rv_similar_content_detail)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                positionItemForTesting,
+                click()
+            )
+        )
     }
 }
