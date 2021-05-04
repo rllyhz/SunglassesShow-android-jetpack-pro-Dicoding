7a00d299e1e1230e43e89bab8a1c0fda2d627f61
rllyhz <rullyihza00@gmail.com>
2021-05-04 17:23:45 +0700

    Updating MainViewModel.kt's methods and logic so that data being kept while configuration changes

--- app/src/main/java/id/rllyhz/sunglassesshow/ui/main/MainViewModel.kt
+++ app/src/main/java/id/rllyhz/sunglassesshow/ui/main/MainViewModel.kt
@@ -1,11 +1,42 @@
 package id.rllyhz.sunglassesshow.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import id.rllyhz.sunglassesshow.data.Movie
+import id.rllyhz.sunglassesshow.data.TVShow
 import id.rllyhz.sunglassesshow.data.source.SunGlassesShowRepository
+import id.rllyhz.sunglassesshow.utils.Resource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainViewModel(
     private val repository: SunGlassesShowRepository
 ) : ViewModel() {
+    private val _firstFetch = MutableLiveData(true)
+
+    private val _movies = MutableLiveData<Resource<List<Movie>>>()
+    val movies: LiveData<Resource<List<Movie>>> = _movies
+
+    private val _tvShows = MutableLiveData<Resource<List<TVShow>>>()
+    val tvShows: LiveData<Resource<List<TVShow>>> = _tvShows
+
+    fun initAllMovies() {
+        if (_firstFetch.value == true)
+            viewModelScope.launch(Dispatchers.Main) {
+                _movies.value = getMovies().value
+                _firstFetch.value = false
+            }
+    }
+
+    fun getAllTVShows() {
+        if (_firstFetch.value == true)
+            viewModelScope.launch(Dispatchers.Main) {
+                _tvShows.value = getTVShows().value
+                _firstFetch.value = false
+            }
+    }
 
     suspend fun getMovies() =
         repository.getMovies()
